cd949640b280ffc95ba705d26078d9433cc2f466
Yessen <y.yesen.y@gmail.com>
2022-11-17 23:56:12 +0900

    Initial Commit

---
+++ SwipableTrial/SwipableTrialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipableTrialApp.swift
+//  SwipableTrial
+//
+//  Created by Yessen Yermukhanbet on 2022/11/17.
+//
+
+import SwiftUI
+
+@main
+struct SwipableTrialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
